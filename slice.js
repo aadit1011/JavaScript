779b1bfbae6f28ed526faf644759194c12dc48e9
Aadit Sharma <106479910+vardan1010@users.noreply.github.com>
2024-03-15 11:40:31 +0545

    Using slice, toUpperCase, toLowerCase, and trim
    Using slice,  toUpperCase, toLowerCase, and trim Function

---
+++ slice.js
@@ -0,0 +1,15 @@
+let name1="Aadit Sharma";
+//To display only the required text we use slice(start,end);
+let name2=name1.slice(0,5);
+console.log(name2);
+let name3=name2.slice(0,3);
+console.log(name3);
+//Using toUpperCase and toLowerCase function
+let name4=name1.toUpperCase();
+let name5=name1.toLowerCase();
+console.log(name4);
+console.log(name5);
+//Using trim function
+let name12="   Aadit Sharma   ";
+let name6=name12.trim();
+console.log(name6);
